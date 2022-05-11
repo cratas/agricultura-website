463d5cdaa18a5022ae4c3e249420f229da51a6d9
cratas <petr.kratochvil.st1@vsb.cz>
2022-05-11 20:52:42 +0200

    added toggle to navbar

--- src/components/navbar/NavbarContainer.js
+++ src/components/navbar/NavbarContainer.js
@@ -1,9 +1,9 @@
 import React from "react";
-import {Container, Nav, Navbar} from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 import MyButton from "../UI/MyButton";
-import classes from './NavbarContainer.module.css';
+import classes from "./NavbarContainer.module.css";
 
 const NavbarContainer = () => {
   return (
@@ -11,13 +11,22 @@ const NavbarContainer = () => {
       <Navbar sticky="top" expand="lg" className={classes.navbarContainer}>
         <Container>
           <Navbar.Brand href="#home">AGROslužby Novotný</Navbar.Brand>
-          <Nav className="ms-auto my-2 my-lg-0">
-            <Link to="/" className={`${classes.links} nav-link`}>Úvod</Link>
-            <Link to="/services" className={`${classes.links} nav-link`}>Služby</Link>
-            <Link to="/gallery" className={`${classes.links} nav-link`}>Galérie</Link>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav className="ms-auto my-2 my-lg-0">
+              <Link to="/" className={`${classes.links} nav-link`}>
+                Úvod
+              </Link>
+              <Link to="/services" className={`${classes.links} nav-link`}>
+                Služby
+              </Link>
+              <Link to="/gallery" className={`${classes.links} nav-link`}>
+                Galérie
+              </Link>
 
-            <MyButton text="Kontakt"/>
-          </Nav>
+              <MyButton text="Kontakt" />
+            </Nav>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
     </>
